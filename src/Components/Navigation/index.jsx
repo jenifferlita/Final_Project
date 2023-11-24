1fcbe6d8d88d5bfcfccf4b24edab57fa2a26cfa3
BrianGustafBirahy <s22110506@student.unklab.ac.id>
2023-11-24 21:27:11 +0800

    memperbaiki komponen navigation

--- src/Components/Navigation/index.jsx
+++ src/Components/Navigation/index.jsx
@@ -38,7 +38,7 @@ const Navigation = () => {
               </li>
               <li>
                 <a className="color_animation" href="#pricing">
-                  PRICING
+                  Menu
                 </a>
               </li>
               <li>
