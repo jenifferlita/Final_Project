7d96189219b6fd9c2ea247a9300d710dcbda711b
makaragessskylie <makarageskyliee@gmail.com>
2023-11-24 22:59:47 +0800

    Menambahkan komponen social section

--- src/pages/homepages.jsx
+++ src/pages/homepages.jsx
@@ -5,6 +5,7 @@ import Pricing from "../Components/Pricing";
 import Footer from "../Components/Footer";
 import Reservation from "../Components/Reservation";
 import Navigation from "../Components/Navigation";
+import SocialSection from "../Components/SocialSection";
 
 const HomePages = () => {
   return (
@@ -34,47 +35,7 @@ const HomePages = () => {
       <Reservation />
 
       {/* ============ Social Section  ============= */}
-      <section className="social_connect">
-        <div className="text-content container">
-          <div className="col-md-6">
-            <span className="social_heading">FOLLOW</span>
-            <ul className="social_icons">
-              <li>
-                <a
-                  className="icon-twitter color_animation"
-                  href="#"
-                  target="_blank"
-                />
-              </li>
-              <li>
-                <a
-                  className="icon-github color_animation"
-                  href="#"
-                  target="_blank"
-                />
-              </li>
-              <li>
-                <a
-                  className="icon-linkedin color_animation"
-                  href="#"
-                  target="_blank"
-                />
-              </li>
-              <li>
-                <a className="icon-mail color_animation" href="#" />
-              </li>
-            </ul>
-          </div>
-          <div className="col-md-4">
-            <span className="social_heading">OR DIAL</span>
-            <span className="social_info">
-              <a className="color_animation" href="tel:883-335-6524">
-                (941) 883-335-6524
-              </a>
-            </span>
-          </div>
-        </div>
-      </section>
+      <SocialSection />
 
       {/* ============ Contact Section  ============= */}
       <Contact />
