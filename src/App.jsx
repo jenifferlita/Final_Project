880b6e65e8f59a19580e79474576935e28dc6655
jenifferlita <s22110064@student.unklab.ac.id>
2023-11-27 21:29:44 +0800

    menambahkan firebase aboutUs

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import './assets/css/bootstrap.css'
-import './assets/css/font-awesome.min.css'
-import './assets/css/jquery-ui.css'
-import './assets/css/main.css'
-import './assets/css/normalize.css'
-import './assets/css/picto-foundry-food.css'
-import './assets/css/style-portfolio.css'
-import HomePages from './pages/homepages'
-
-const App=() =>{
-
+import "./assets/css/bootstrap.css";
+import "./assets/css/font-awesome.min.css";
+import "./assets/css/jquery-ui.css";
+import "./assets/css/main.css";
+import "./assets/css/normalize.css";
+import "./assets/css/picto-foundry-food.css";
+import "./assets/css/style-portfolio.css";
+import HomePages from "./pages/homepages";
+import "./Config/Firebase";
+import React from "react";
+const App = () => {
   return (
     <>
-    <HomePages/>
+      <HomePages />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Config/Firebase/index.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB55a4_orQNIx12IinbkAu-89niGJoDF7I",
+  authDomain: "frontend-a-f7fa7.firebaseapp.com",
+  databaseURL: "https://frontend-a-f7fa7-default-rtdb.firebaseio.com",
+  projectId: "frontend-a-f7fa7",
+  storageBucket: "frontend-a-f7fa7.appspot.com",
+  messagingSenderId: "780546558467",
+  appId: "1:780546558467:web:574a3d4e2b724d3a681c36",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
