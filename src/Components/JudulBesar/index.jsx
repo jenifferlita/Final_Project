0a3a31c6027facdd90483ae23554e5c7405da0af
jenifferlita <s22110064@student.unklab.ac.id>
2023-11-28 13:27:46 +0800

    menambahkan firebase JudulBesar

--- src/Components/JudulBesar/index.jsx
+++ src/Components/JudulBesar/index.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
+import { getDatabase, ref, child, get } from "firebase/database";
+
 const Judul = () => {
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    const dbRef = ref(getDatabase());
+
+    get(child(dbRef, `JudulBesar`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          setTitle(data.title);
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
   return (
     <section id="top" className="description_content">
       <div className="kantin background_content">
         <a className="judul-utama" href="#">
-          U-Canteen
+          {title}
         </a>
       </div>
       <div className="text-content container"></div>
